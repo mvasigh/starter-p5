5c9da410b5e29c088cbd9a91ce5610d33c6d5e75
Mehdi Vasigh <mehdi.vasigh@gmail.com>
2021-03-06 08:49:24 -0500

    better app structure

--- src/app/P5.jsx
+++ src/app/P5.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from "preact/hooks";
 import p5 from "p5";
-import sketch from "./sketch";
+import sketch from "../sketch";
 
 const WS_URL = import.meta.env.VITE_WS_URL ?? "ws://localhost:1234";
 
@@ -13,13 +13,22 @@ export default () => {
     if (!drawing.onmessage) return;
 
     const socket = new WebSocket(WS_URL);
+
     socket.addEventListener("message", (event) => {
-      drawing.onmessage(event);
+      const { oscMsg, timeTag } = JSON.parse(event.data);
+
+      const message = {
+        timeTag,
+        address: oscMsg.address,
+        args: oscMsg.args.map((arg) => arg.value),
+      };
+
+      drawing.onmessage(message);
     });
 
     return () => {
       socket.close();
-    }
+    };
   }, []);
 
   return <div id="p5-sketch" ref={sketchRef} />;
